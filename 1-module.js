4a4f8e5464190a46808d679e55121d3f3c5bb324
sweakyf <115627830+sweakyf@users.noreply.github.com>
2022-11-25 19:24:07 +1000

    Create 1-module.js

--- 1-module.js
+++ 1-module.js
@@ -8,24 +8,12 @@ function getRandomArrayElement(array) {
     return array[getRandomInt(0, array.length - 1)];
 }
 
-function getComment(count) {
-    return [
-        {
-            id: getRandomInt(1, MAX_COMMENT_COUNT * count),
-            message: getRandomArrayElement(messages),
-            user: {
-                avatar: `./img/avatars/${getRandomInt(1, MAX_AVATAR_COUNT)}.svg`,
-                name: getRandomArrayElement(usernames),
-            }
-        }
-    ]
-}
-
-const MAX_COMMENT_COUNT = 10;
+const MAX_COMMENT_COUNT = 5;
 const MAX_AVATAR_COUNT = 6;
 const MAX_PHOTO_COUNT = 25;
 const MIN_LIKE_COUNT = 15;
 const MAX_LIKE_COUNT = 200;
+
 
 const descriptions = [
     'красивая картинка',
@@ -49,31 +37,68 @@ const usernames = [
     'Антон',
 ];
 
+const usedCommentIds = [];
+
+function getComment(maxPictureId) {
+    let commentId = getRandomInt(1, MAX_COMMENT_COUNT * maxPictureId);
+    while (usedCommentIds.includes(commentId)) {
+        commentId = getRandomInt(1, MAX_COMMENT_COUNT * maxPictureId);
+    }
+
+    usedCommentIds.push(commentId);
+
+    return {
+        id: commentId,
+        message: getRandomArrayElement(messages),
+        user: {
+            avatar: `./img/avatar-${getRandomInt(1, MAX_AVATAR_COUNT)}.svg`,
+            name: getRandomArrayElement(usernames),
+        }
+    };
+}
+
+const usedPictureIds = [];
+
+function getPicture(maxPictureId) {
+    const comments = [];
+    const commentCount = getRandomInt(0, MAX_COMMENT_COUNT);
+
+    while (comments.length < commentCount) {
+        comments.push(getComment(maxPictureId));
+    }
+
+    let pictureId = getRandomInt(1, maxPictureId);
+    while (usedPictureIds.includes(pictureId)) {
+        pictureId = getRandomInt(1, maxPictureId);
+    }
+
+    usedPictureIds.push(pictureId);
+
+    return {
+        id: pictureId,
+        url: `./photos/${getRandomInt(1, MAX_PHOTO_COUNT)}.jpg`,
+        description: getRandomArrayElement(descriptions),
+        likes: getRandomInt(MIN_LIKE_COUNT, MAX_LIKE_COUNT),
+        comments: comments
+    }
+}
+
 function getPictures(count) {
     const arrayPictures = [];
 
     while (arrayPictures.length < count) {
-        const picture = {
-            id: getRandomInt(1, count),
-            url: `./photos/${getRandomInt(1, MAX_PHOTO_COUNT)}.jpg`,
-            description: getRandomArrayElement(descriptions),
-            likes: getRandomInt(MIN_LIKE_COUNT, MAX_LIKE_COUNT),
-            comments: [
-                getComment(count)
-            ]
-        }
-        arrayPictures.push(picture)
+        arrayPictures.push(getPicture(count))
     }
 
     return arrayPictures;
 }
 
-console.log(getPictures(10));
+console.log(getPictures(2));
 
-const getItem = function () {
-    return {};
-};
+// не трогать
+// const getItem = function () {
+//     return {};
+// };
 
-const items = new Array(10).fill(null).map(() => getItem());
-
-console.log(items);
+// const items1 = new Array(10).fill(null).map(() => getItem());
+// const items2 = Array.from({length: 10}, () => getItem());
